8fdf07f0f3afd26f714f856522e83079d0f3cfbb
unknown <amhe3@697-334-Theoden.sf.k12.internal>
2015-09-02 10:45:11 -0700

    experimenting rwith class

--- Lightning.pde
+++ Lightning.pde
@@ -1,8 +1,8 @@
 
-int startX = 150;
+/*int startX = 150;
 int startY = 0;
 int endX = 150;
-int endY = 0;
+int endY = 0;*/
 
 void setup()
 {
@@ -12,19 +12,56 @@ void setup()
 }
 void draw()
 {
-	while(endY<300)
+	/*while(endY<300)
 	{
 		endY = startY + ((int) (Math.random()*9));
 		endX = startX + ((int) ((Math.random()*18)-9));
-		stroke((int) (Math.random()*50)+125);
+		stroke((int) (Math.random()*100)+100, (int) (Math.random()*100)+100, (int) (Math.random()*100)+100);
 		line(startX, startY, endX, endY);
 		startX = endX;
 		startY = endY;
-	}
+	}*/
+	Lightning middle = new Lightning(150, 0);
+	Lightning left = new Lightning(10, 0);
+	Lightning right = new Lightning(190, 0);
+	middle.bolt();
+	left.bolt();
+	right.bolt();
 	noStroke();
   	fill(0, 100, 0);
   	rect(0, 275, 300, 25); //grass
+  	fill(139, 137, 137);
+  	rect(0, 0, 300, 13); //cloud
 }
+
+class Lightning
+{
+	/*int startX = 150;
+	int startY = 0;
+	int endX = 150;
+	int endY = 0;*/
+	int startX, startY, endX, endY;
+	Lightning (int x, int y)
+	{
+		startX = x;
+		endX = x;
+		startY = y;
+		endY = y;
+	}
+	void bolt()
+	{
+		while(endY<300)
+		{
+			endY = startY + ((int) (Math.random()*9));
+			endX = startX + ((int) ((Math.random()*18)-9));
+			stroke((int) (Math.random()*100)+100, (int) (Math.random()*100)+100, (int) (Math.random()*100)+100);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}
+}
+
 void mousePressed()
 {
 	background(25, 25, 112);
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -15,10 +15,10 @@ import java.io.IOException;
 public class Lightning extends PApplet {
 
 
-int startX = 150;
-int startY = 150;
-int endX = 0;
-int endY = 0;
+/*int startX = 150;
+int startY = 0;
+int endX = 150;
+int endY = 0;*/
 
 public void setup()
 {
@@ -28,19 +28,56 @@ public void setup()
 }
 public void draw()
 {
-	while(endY<300)
+	/*while(endY<300)
 	{
 		endY = startY + ((int) (Math.random()*9));
 		endX = startX + ((int) ((Math.random()*18)-9));
-		stroke((int) (Math.random()*50)+125);
+		stroke((int) (Math.random()*100)+100, (int) (Math.random()*100)+100, (int) (Math.random()*100)+100);
 		line(startX, startY, endX, endY);
 		startX = endX;
 		startY = endY;
-	}
-	  noStroke();
-  fill(0, 100, 0);
-  rect(0, 275, 300, 25); //grass
+	}*/
+	Lightning middle = new Lightning(150, 0);
+	Lightning left = new Lightning(10, 0);
+	Lightning right = new Lightning(190, 0);
+	middle.bolt();
+	left.bolt();
+	right.bolt();
+	noStroke();
+  	fill(0, 100, 0);
+  	rect(0, 275, 300, 25); //grass
+  	fill(139, 137, 137);
+  	rect(0, 0, 300, 13); //cloud
 }
+
+class Lightning
+{
+	/*int startX = 150;
+	int startY = 0;
+	int endX = 150;
+	int endY = 0;*/
+	int startX, startY, endX, endY;
+	Lightning (int x, int y)
+	{
+		startX = x;
+		endX = x;
+		startY = y;
+		endY = y;
+	}
+	public void bolt()
+	{
+		while(endY<300)
+		{
+			endY = startY + ((int) (Math.random()*9));
+			endX = startX + ((int) ((Math.random()*18)-9));
+			stroke((int) (Math.random()*100)+100, (int) (Math.random()*100)+100, (int) (Math.random()*100)+100);
+			line(startX, startY, endX, endY);
+			startX = endX;
+			startY = endY;
+		}
+	}
+}
+
 public void mousePressed()
 {
 	background(25, 25, 112);
